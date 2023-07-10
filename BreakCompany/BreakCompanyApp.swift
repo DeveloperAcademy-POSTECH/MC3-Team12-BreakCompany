43e8dcca1b51bd11f5b145f42a147dee7b2e8761
hajime <impapa68@gmail.com>
2023-07-11 04:00:21 +0900

    [Add] initial project

---
+++ BreakCompany/BreakCompanyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakCompanyApp.swift
+//  BreakCompany
+//
+//  Created by Ha Jong Myeong on 2023/07/11.
+//
+
+import SwiftUI
+
+@main
+struct BreakCompanyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
