a4695d67622acae93f7fef2d11f7317f9b50af0e
hajime <impapa68@gmail.com>
2023-07-12 14:01:03 +0900

    [Chore] Delete Project for Initial Setup

--- PeepPeep/PeepPeep/Common/Themes/Colors.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Colors.swift
-//  PeepPeep
-//
-//  Created by Ha Jong Myeong on 2023/07/11.
-//
-
-import Foundation
--- PeepPeep/PeepPeep/Common/Themes/Styles.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Fonts.swift
-//  PeepPeep
-//
-//  Created by Ha Jong Myeong on 2023/07/11.
-//
-
-import Foundation
