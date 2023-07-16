5a24bd197eb31d7e45cc15b435b1d51569ed053b
yejink0328 <129183269+yejink0328@users.noreply.github.com>
2023-07-16 15:37:56 +0900

    [Feat/#27] 달력 형태 구현

--- PeepPeep/PeepPeep/View/DailyFeedBack/DailyFeedBackView.swift
+++ PeepPeep/PeepPeep/View/DailyFeedBack/DailyFeedBackView.swift
@@ -8,13 +8,104 @@
 import SwiftUI
 
 struct DailyFeedBackView: View {
+    @State var month: Date
+    let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 7)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HeaderView
+            CalendarGridView
+        }
+        .padding(20)
     }
+    
+    private var HeaderView: some View {
+        return VStack {
+            Text(month, formatter: Self.dateFormatter)
+                .font(.title)
+            LazyVGrid(columns: columns, spacing: 2) {
+                ForEach(Self.weekdaySymbols.indices, id: \.self) { week in
+                    Text("\(Self.weekdaySymbols[week])")
+                        .frame(width: 50, height: 50)
+                }
+            }
+        }
+    }
+    
+    private var CalendarGridView: some View {
+        let daysInMonth: Int = numberOfDays(in: month)
+        let firstWeekday: Int = firstWeekdayOfMonth(in: month) - 1
+        
+        return VStack {
+            LazyVGrid(columns: columns, spacing: 2) {
+                ForEach(0 ..< firstWeekday, id: \.self) { _ in
+                    Text("")
+                        .frame(width: 50, height: 50)
+                }
+                
+                ForEach(1 ..< daysInMonth + 1, id: \.self) { day in
+                    CellView(day: day)
+                }
+            }
+        }
+    }
+}
+
+struct CellView: View {
+    @State var day: Int
+    
+    init(day: Int) {
+        self.day = day
+    }
+    
+    var body: some View {
+        ZStack {
+            Text("\(day)")
+                .frame(width: 50, height: 50)
+                .cornerRadius(3)
+        }
+    }
+}
+
+private extension DailyFeedBackView {
+    private func getDate(for day: Int) -> Date {
+        return Calendar.current.date(byAdding: .day, value: day, to: startOfMonth())!
+    }
+    
+    func startOfMonth() -> Date {
+        let components = Calendar.current.dateComponents([.year, .month], from: month)
+        return Calendar.current.date(from: components)!
+    }
+    
+    func numberOfDays(in date: Date) -> Int {
+        return Calendar.current.range(of: .day, in: .month, for: date)?.count ?? 0
+    }
+    
+    func firstWeekdayOfMonth(in date: Date) -> Int {
+        let components = Calendar.current.dateComponents([.year, .month], from: date)
+        let firstDayOfMonth = Calendar.current.date(from: components)!
+        
+        return Calendar.current.component(.weekday, from: firstDayOfMonth)
+    }
+}
+
+private extension DailyFeedBackView {
+    static let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.dateFormat = "yyyy년 M월"
+        return formatter
+    }()
+    
+    static let weekdaySymbols: [String] = {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        return formatter.shortWeekdaySymbols
+    }()
 }
 
 struct DailyFeedBackView_Previews: PreviewProvider {
     static var previews: some View {
-        DailyFeedBackView()
+        DailyFeedBackView(month: Date())
     }
 }
