8ff6594377ff323d4240c11cacd8f08e35424b27
hajime <impapa68@gmail.com>
2023-07-17 16:33:54 +0900

    [Feat/#24] ScreenTime 연동 및 기본 통계 컴포넌트 구현

---
+++ PeepPeep/FeedBack/FeedBack.swift
@@ -0,0 +1,20 @@
+//
+//  FeedBack.swift
+//  FeedBack
+//
+//  Created by Ha Jong Myeong on 2023/07/12.
+//
+
+import DeviceActivity
+import SwiftUI
+
+@main
+struct DeviceActivityExtension: DeviceActivityReportExtension {
+    var body: some DeviceActivityReportScene {
+        // Create a report for each DeviceActivityReport.Context that your app supports.
+        TotalActivityReport { totalActivity in
+            TotalActivityView(activityReport: totalActivity)
+        }
+        // Add more reports here...
+    }
+}
---
+++ PeepPeep/PeepPeep/View/InitialSetupScreen/ActivitySummaryView.swift
@@ -0,0 +1,29 @@
+//
+//  ActivitySummaryView.swift
+//  PeepPeep
+//
+//  Created by Ha Jong Myeong on 2023/07/12.
+//
+
+import SwiftUI
+import DeviceActivity
+
+struct ActivitySummaryView: View {
+    @State private var totalActivityContext: DeviceActivityReport.Context = .init(rawValue: "Total Activity")
+    @State private var filter = DeviceActivityFilter(
+        segment: .daily(
+            during: Calendar.current.dateInterval(
+                of: .day, for: .now
+            )!
+        ),
+        users: .all,
+        devices: .init([.iPhone, .iPad])
+    )
+    
+    var body: some View {
+        ZStack {
+            STProgressView()
+            DeviceActivityReport(totalActivityContext, filter: filter)
+        }
+    }
+}
