abd1d153e12440a9add335c731f6a644dc81689f
suri0000 <yesle2005@naver.com>
2023-07-21 00:39:01 +0900

    [Feat/#33] 이미지와 텍스트
    [Add/#33] Font file "DOSSaemmul.ttf"
    
    [Add/#33] Image file "Box Off"

--- PeepPeep/PeepPeep/View/SplashScreen/SplashView.swift
+++ PeepPeep/PeepPeep/View/SplashScreen/SplashView.swift
@@ -8,7 +8,17 @@ import SwiftUI
 
 struct SplashView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("이상한 아저씨가 학교 앞에서 무엇인가 팔고 있다")
+                .font(.custom("DOSSaemmul", size: 18))
+                .lineSpacing(19)
+                .multilineTextAlignment(.center)
+                .frame(width: 300)
+                
+            Image("BoxOff")
+                .resizable()
+                .frame(width: 300, height: 350)
+        }
     }
 }
 
