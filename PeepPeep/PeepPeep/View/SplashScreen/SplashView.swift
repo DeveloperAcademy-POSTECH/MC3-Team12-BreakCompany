7bc9172e7d104170c42c584737138a590724861d
suri0000 <yesle2005@naver.com>
2023-07-21 03:42:42 +0900

    [Feat/#33] TabView로 page indicator 표시

--- PeepPeep/PeepPeep/View/SplashScreen/SplashView.swift
+++ PeepPeep/PeepPeep/View/SplashScreen/SplashView.swift
@@ -8,17 +8,33 @@ import SwiftUI
 
 struct SplashView: View {
     var body: some View {
-        VStack {
-            Text("이상한 아저씨가 학교 앞에서 무엇인가 팔고 있다")
-                .font(.custom("DOSSaemmul", size: 18))
-                .lineSpacing(19)
-                .multilineTextAlignment(.center)
-                .frame(width: 300)
+        TabView{
+            VStack {
+                Text("이상한 아저씨가 학교 앞에서 무엇인가 팔고 있다")
+                    .font(.custom("DOSSaemmul", size: 17))
+                    .lineSpacing(19)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 300)
+                    
+                Image("BoxOff")
+                    .resizable()
+                    .frame(width: 300, height: 350)
+            }
+            
+            VStack {
+                Text("앗 병아리잖아? 옛날부터 너무 키워보고 싶었는데...")
+                    .font(.custom("DOSSaemmul", size: 17))
+                    .lineSpacing(19)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 300)
                 
-            Image("BoxOff")
-                .resizable()
-                .frame(width: 300, height: 350)
+                
+            }
+            
         }
+        .tabViewStyle(.page)
+        .indexViewStyle(.page(backgroundDisplayMode: .always))
+        
     }
 }
 
