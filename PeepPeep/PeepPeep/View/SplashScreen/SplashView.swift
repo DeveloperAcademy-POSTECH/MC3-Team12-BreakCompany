a7278dfe84e9814d316d27200d4ed942888de9a5
suri0000 <yesle2005@naver.com>
2023-07-23 20:44:11 +0900

    [Feat/#33] image, text 배치 완료

--- PeepPeep/PeepPeep/View/SplashScreen/SplashView.swift
+++ PeepPeep/PeepPeep/View/SplashScreen/SplashView.swift
@@ -9,64 +9,74 @@ import SwiftUI
 struct SplashView: View {
     var body: some View {
         TabView{
-            VStack {
+            ZStack {
                 Text("이상한 아저씨가 학교 앞에서 무엇인가 팔고 있다")
                     .font(.custom("DOSSaemmul", size: 17))
                     .lineSpacing(19)
                     .multilineTextAlignment(.center)
                     .frame(width: 300)
+                    .offset(y: -200)
                 
                 Image("BoxOff")
                     .resizable()
-                    .frame(width: 300, height: 350)
+                    .frame(width: 300, height: 300)
+                    .offset(y: 50)
             }
             
-            VStack {
+            ZStack {
                 Text("앗 병아리잖아?\n옛날부터 너무 키워보고 싶었는데...")
                     .font(.custom("DOSSaemmul", size: 17))
                     .lineSpacing(19)
                     .multilineTextAlignment(.center)
                     .frame(width: 300)
+                    .offset(y: -200)
                 
                 Image("Base1")
                     .resizable()
-                    .frame(width: 400, height: 400)
+                    .frame(width: 300, height: 300)
+                    .offset(y: 50)
             }
             
-            VStack {
+            ZStack {
                 Text("\"병아리는 많이 만지면 아프단다.\"")
                     .font(.custom("DOSSaemmul", size: 17))
                     .lineSpacing(19)
                     .multilineTextAlignment(.center)
                     .frame(width: 300)
+                    .offset(y: -200)
                 
                 Image("Base1")
                     .resizable()
-                    .frame(width: 400, height: 400)
+                    .frame(width: 300, height: 300)
+                    .offset(y: 50)
             }
             
-            VStack {
+            ZStack {
                 Text("\"핸드폰을 만지는 동안\n병아리가 스트레스를 받으니,\n주의해서 잘 키워주거라!\"")
                     .font(.custom("DOSSaemmul", size: 17))
                     .lineSpacing(19)
                     .multilineTextAlignment(.center)
                     .frame(width: 300)
+                    .offset(y: -200)
                 
                 Image("Base1")
                     .resizable()
-                    .frame(width: 400, height: 400)
+                    .frame(width: 300, height: 300)
+                    .offset(y: 50)
             }
             
-            VStack {
+            ZStack {
                 Text("우리집으로 가장!")
                     .font(.custom("DOSSaemmul", size: 17))
                     .lineSpacing(19)
                     .multilineTextAlignment(.center)
                     .frame(width: 300)
+                    .offset(y: -200)
                 
                 Image("BoxOn")
                     .resizable()
-                    .frame(width: 280, height: 280)
+                    .frame(width: 300, height: 300)
+                    .offset(y: 50)
             }
             
         }
@@ -75,6 +85,7 @@ struct SplashView: View {
         .onAppear {
             setupAppearance()
         }
+        .padding(.bottom, 50)
         
     }
     
