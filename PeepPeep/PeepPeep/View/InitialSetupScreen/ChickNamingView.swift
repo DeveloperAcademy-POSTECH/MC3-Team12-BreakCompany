31f0ac089fb348abf1b3db1c0229185594e6e222
hajime <impapa68@gmail.com>
2023-07-26 00:04:30 +0900

    [Feat/#32] Refactor ChickNamingView

--- PeepPeep/PeepPeep/View/InitialSetupScreen/ChickNamingView.swift
+++ PeepPeep/PeepPeep/View/InitialSetupScreen/ChickNamingView.swift
@@ -10,32 +10,79 @@ import PeepPeepCommons
 
 struct ChickNamingView: View {
     @ObservedObject var viewModel = ChickNamingViewModel()
-
     @State private var name = ""
 
     var body: some View {
-        NavigationStack {
+        NavigationView {
             VStack {
-                Text("병아리의 이름을 지어주세요")
-                    .font(.dosSsaemmul(size: 20))
-                TextField("병아리", text: $name)
-                    .textFieldStyle(UnderlinedTextFieldStyle())
-                    .multilineTextAlignment(.center)
-                    .font(.dosSsaemmul(size: 20))
-                Image("chick")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 200, height: 200)
-                Button(action: {
-                    viewModel.updateChickName(name: name)
-                }) {
-                    NavigationLink(destination: ScreenTimeRequestView()) {
-                        Text("결정")
-                    }
-                }
-                .buttonStyle(CommonButtonStyle(paddingSize: 30))
+                TitleText()
+                ChickNameTextField(name: $name)
+                ChickImageView()
+                DecisionButton(viewModel: viewModel, name: name)
             }
             .padding()
+            .navigationBarBackButtonHidden(true)
+            .navigationBarItems(trailing: SkipButton(viewModel: viewModel, name: name))
+            .font(.dosSsaemmul(size: 20))
+            .foregroundColor(Color.black)
+        }
+    }
+}
+
+struct TitleText: View {
+    var body: some View {
+        Text("병아리의 이름을 지어주세요")
+            .font(.dosSsaemmul(size: 20))
+    }
+}
+
+struct ChickNameTextField: View {
+    @Binding var name: String
+
+    var body: some View {
+        TextField("병아리", text: $name)
+            .textFieldStyle(UnderlinedTextFieldStyle())
+            .multilineTextAlignment(.center)
+            .font(.dosSsaemmul(size: 20))
+    }
+}
+
+struct ChickImageView: View {
+    var body: some View {
+        Image("chick")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 200, height: 200)
+    }
+}
+
+struct DecisionButton: View {
+    let viewModel: ChickNamingViewModel
+    let name: String
+
+    var body: some View {
+        Button(action: {
+            viewModel.updateChickName(name: name)
+        }) {
+            NavigationLink(destination: ScreenTimeRequestView()) {
+                Text("결정")
+            }
+        }
+        .buttonStyle(CommonButtonStyle(paddingSize: 30))
+    }
+}
+
+struct SkipButton: View {
+    let viewModel: ChickNamingViewModel
+    let name: String
+
+    var body: some View {
+        Button(action: {
+            viewModel.updateChickName(name: name)
+        }) {
+            NavigationLink(destination: ScreenTimeRequestView()) {
+                Text("Skip")
+            }
         }
     }
 }
