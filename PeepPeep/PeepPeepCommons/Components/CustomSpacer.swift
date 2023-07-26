61dcad7d2716b888a592f9bc2afa988c03737013
Hajime <impapa68@gmail.com>
2023-07-26 23:03:35 +0900

    [Fix/#53] Add CustomSpacer

---
+++ PeepPeep/PeepPeepCommons/Components/CustomSpacer.swift
@@ -0,0 +1,20 @@
+//
+//  CustomSpacer.swift
+//  PeepPeepCommons
+//
+//  Created by Ha Jong Myeong on 2023/07/26.
+//
+import SwiftUI
+
+public struct CustomSpacer: View {
+    public var height: CGFloat
+
+    public init(height: CGFloat) {
+        self.height = height
+    }
+
+    public var body: some View {
+        Spacer()
+            .frame(height: height)
+    }
+}
