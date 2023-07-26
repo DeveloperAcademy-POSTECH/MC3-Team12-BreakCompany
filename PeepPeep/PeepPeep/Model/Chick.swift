5c783518587dae8310377b307a802d654f9044d3
Hajime <impapa68@gmail.com>
2023-07-26 22:49:13 +0900

    [Fix/#53] Implement Chick Model

--- PeepPeep/PeepPeep/Model/Chick.swift
+++ PeepPeep/PeepPeep/Model/Chick.swift
@@ -2,7 +2,10 @@
 //  Chick.swift
 //  PeepPeep
 //
-//  Created by Ha Jong Myeong on 2023/07/26.
+//  Created by Ha Jong Myeong on 2023/07/24.
 //
-
 import Foundation
+
+struct Chick {
+    var name: String
+}
