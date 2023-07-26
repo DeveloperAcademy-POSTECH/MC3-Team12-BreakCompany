9c0bb09b617aab9ecf1fcd58b35c2501c46b8308
Hajime <impapa68@gmail.com>
2023-07-26 23:37:55 +0900

    [Fix/#53] Link InitialSetup with MainView

--- PeepPeep/PeepPeep/View/InitialSetupScreen/ActivitySummaryView.swift
+++ PeepPeep/PeepPeep/View/InitialSetupScreen/ActivitySummaryView.swift
@@ -13,6 +13,7 @@ struct ActivitySummaryView: View {
     @EnvironmentObject var model: ScreenTimeAppSelection
     @State var isPresented = false
     @State private var totalActivityContext: DeviceActivityReport.Context = .init(rawValue: "Total Activity")
+    @State private var navigateToMain = false
     @State private var filter: DeviceActivityFilter = {
         // 현재 날짜를 불러올 수 없다면, 이전 24시간의 기준으로 날짜의 사용시간 데이터를 받아올 수 있도록 설정
         let now = Date()
@@ -32,8 +33,15 @@ struct ActivitySummaryView: View {
             Spacer()
             DeviceActivityReport(totalActivityContext, filter: filter)
             Button("확인") { isPresented = true }
-                .familyActivityPicker(isPresented: $isPresented, selection: $model.newSelection)
                 .buttonStyle(CommonButtonStyle(paddingSize: 94))
+                .familyActivityPicker(isPresented: $isPresented, selection: $model.newSelection)
+                .onChange(of: model.newSelection) { _ in
+                    navigateToMain = true
+                }
+
+                NavigationLink(destination: MainView(), isActive: $navigateToMain) {
+                    EmptyView()
+                }
             CustomSpacer(height: 30)
         }
         .navigationBarBackButtonHidden(true)
