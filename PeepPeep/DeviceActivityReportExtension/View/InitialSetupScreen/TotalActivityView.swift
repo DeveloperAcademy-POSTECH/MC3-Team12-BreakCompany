b4cd60a8a0a6eb32f7c9e7ec3ec127cc25428f36
Hajime <impapa68@gmail.com>
2023-07-28 21:40:17 +0900

    [Fix/#58] TotalActivityView 수정

--- PeepPeep/DeviceActivityReportExtension/View/InitialSetupScreen/TotalActivityView.swift
+++ PeepPeep/DeviceActivityReportExtension/View/InitialSetupScreen/TotalActivityView.swift
@@ -1,6 +1,5 @@
-//
 //  TotalActivityView.swift
-//  DeviceActuvityReportExtension
+//  DeviceActivityReportExtension
 //
 //  Created by Ha Jong Myeong on 2023/07/12.
 //
@@ -12,70 +11,82 @@ import SwiftUI
 struct TotalActivityView: View {
     var activityReport: ActivityReport
 
+    /// PieChartView에서 사용할 차트 데이터를 반환
+    var chartData: [(Double, Color)] {
+        activityReport.apps.enumerated().map { (index, app) in
+            (Double(app.duration), pieChartColorPalette[index % pieChartColorPalette.count])
+        }
+    }
+
     var body: some View {
         VStack {
-            PieChart(activityReport: activityReport)
-            SelectionInstruction()
-            AppActivityList(activityReport: activityReport)
-        }
-    }
-}
-
-/// 파이차트 뷰
-struct PieChart: View {
-    var activityReport: ActivityReport
-    var body: some View {
-        ZStack {
-            PieChartView(data: activityReport.chartData)
-            VStack {
-                Text("어제 총 사용시간")
-                    .font(.dosSsaemmul(size: 17))
-                    .padding(.bottom, 3)
-                Text("\(activityReport.totalDuration.stringFromTimeInterval())")
-                    .font(.dosSsaemmul(size: 25))
+            // 파이 차트 뷰와 총 활동 시간을 보여주는 텍스트 뷰를 중첩
+            ZStack {
+                PieChartView(data: chartData)
+                TotalDurationText(duration: activityReport.totalDuration)
             }
+            InstructionText()
+            // 앱 별 Activity 리스트
+            ActivityList(activities: activityReport.apps)
         }
     }
 }
 
-/// 햅 활동 정보 리스트 행
-struct ListRow: View {
-    var eachApp: AppDeviceActivity
+/// 총 사용 시간
+struct TotalDurationText: View {
+    var duration: TimeInterval
 
     var body: some View {
-        HStack {
-            Rectangle()
-                .fill(eachApp.color)
-                .frame(width: 5, height: 10)
-                .cornerRadius(5)
-            Label(eachApp.iconToken).labelStyle(.iconOnly)
-            Text(eachApp.displayName)
-            Spacer()
-            Text(eachApp.duration.stringFromTimeInterval())
+        VStack {
+            Text("어제 총 사용시간")
+                .padding(.bottom, 3)
+                .font(.dosSsaemmul(size: 17))
+            Text("\(duration.stringFromTimeInterval())")
+                .font(.dosSsaemmul(size: 20))
         }
     }
 }
 
-/// 앱 선택 안내 문구
-struct SelectionInstruction: View {
+/// 안내 문구
+struct InstructionText: View {
     var body: some View {
         Text("선택하기 버튼을 눌러\n사용 시간을 줄이고 싶은 앱을 선택해주세요")
             .multilineTextAlignment(.center)
             .lineSpacing(5)
-            .font(.dosSsaemmul(size: 15))
+            .font(.dosSsaemmul(size: 17))
     }
 }
 
-/// 앱 활동 정보 리스트
-struct AppActivityList: View {
-    var activityReport: ActivityReport
+/// 앱 Activity 리스트
+struct ActivityList: View {
+    var activities: [AppDeviceActivity]
 
     var body: some View {
-        List(activityReport.apps.indices, id: \.self) { index in
-            ListRow(eachApp: activityReport.apps[index])
-                .font(.dosSsaemmul(size: 17))
+        List(activities.indices, id: \.self) { index in
+            ActivityRow(eachApp: activities[index], color: pieChartColorPalette[index % pieChartColorPalette.count])
         }
         .background(.white)
         .scrollContentBackground(.hidden)
+    }
+}
+
+/// 앱 Activity 리스트 상세(행)
+struct ActivityRow: View {
+    var eachApp: AppDeviceActivity
+    var color: Color
+
+    var body: some View {
+        HStack {
+            Rectangle()
+                .fill(color)
+                .frame(width: 5, height: 10)
+                .cornerRadius(5)
+            Label(eachApp.iconToken).labelStyle(.iconOnly)
+            Text(eachApp.displayName)
+                .font(.dosSsaemmul(size: 17))
+            Spacer()
+            Text(eachApp.duration.stringFromTimeInterval())
+                .font(.dosSsaemmul(size: 17))
+        }
     }
 }
