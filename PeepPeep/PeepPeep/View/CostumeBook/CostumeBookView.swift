402e499a4c0139a8914d32b005dff17c1c5a0612
yejink0328 <129183269+yejink0328@users.noreply.github.com>
2023-07-29 21:44:56 +0900

    [Feat/#44] Bottom Sheet 나가기 버튼 및 Costume 해금 조건에 따라 이미지 바뀌기 구현 완료

--- PeepPeep/PeepPeep/View/CostumeBook/CostumeBookView.swift
+++ PeepPeep/PeepPeep/View/CostumeBook/CostumeBookView.swift
@@ -7,13 +7,79 @@
 
 import SwiftUI
 
+struct CostumeCell: Identifiable {
+    let id = UUID()
+    let cellNum: Int
+    let imageName: String
+    let imageTitle: String
+    let imageExplain: String
+    var isUnlocked: Bool
+    
+    init(cellNum: Int, imageName: String, imageTitle: String, imageExplain: String, isUnlocked: Bool) {
+        self.cellNum = cellNum
+        self.imageName = imageName
+        self.imageTitle = imageTitle
+        self.imageExplain = imageExplain
+        self.isUnlocked = isUnlocked
+    }
+}
+
 struct CostumesBookView: View {
-    let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 2)
+    
+    let isUnlockedImages = [
+        false, true, false, true,
+        true, false, false, true,
+        true, true
+    ]
+    
     let costumeNames: Array = ["Normal", "Cake", "Moon", "Music", "Newbie", "Clover", "Apple", "Bag1"]
     
-    @State var isBottomSheet: Bool = false
-    @State var costumeName: String = ""
-    @State var isSelect: Bool = false
+    let costumeShadowNames: Array = ["NormalShadow", "CakeShadow", "MoonShadow", "MusicShadow", "NewbieShadow", "CloverShadow", "AppleShadow", "Bag1Shadow"]
+    
+    let costumeTitles: [String] = [
+        "클래식",
+        "우리 백 일이에요",
+        "한 달 살기",
+        "무병 장수",
+        "시작이 반이다",
+        "슬롯 머신",
+        "얼리어답터",
+        "돌아온 탕아"
+    ]
+    
+    let costumeExplains: [String] = [
+        "기본으로 제공되는 병아리입니다.",
+        "설정한 핸드폰 사용시간을 잘 지킨날이 100일이 되면 지급됩니다.",
+        "한 달 연속으로 핸드폰 목표 사용 시간을 잘 지켰을 때 지급됩니다.",
+        "설정한 핸드폰 사용시간을 일주일 연속으로 잘 지켰을 때 지급됩니다.",
+        "설정한 핸드폰 사용시간을 1일 잘 지키면 지급됩니다.",
+        "3일 연속으로 병아리의 스트레스 지수를 33.3%로 유지했을 때 지급됩니다.",
+        "발표날 앱을 설치하시면 지급됩니다.",
+        "설정한 핸드폰 사용시간을 지키지 못해 병아리가 처음 가출했을 때 지급됩니다."
+    ]
+    
+    var cells: [CostumeCell] {
+        var result: [CostumeCell] = []
+        for i in 0...7 {
+            let isUnlockedImage = isUnlockedImages[i]
+            let imageName = (isUnlockedImage ? costumeNames[i] : costumeShadowNames[i])
+            let imageTitle = costumeTitles[i]
+            let imageExplain = costumeExplains[i]
+            
+            result.append(
+                CostumeCell(cellNum: i,
+                            imageName: imageName,
+                            imageTitle: imageTitle,
+                            imageExplain: imageExplain,
+                            isUnlocked: isUnlockedImage)
+            )
+        }
+        return result
+    }
+    
+    let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 2)
+    
+    @State var selectNum: Int = 0
     
     var body: some View {
         VStack {
@@ -22,113 +88,99 @@ struct CostumesBookView: View {
     }
     
     private var CostumeGridView: some View {
-
+        
         return VStack {
             ScrollView {
-                LazyVGrid(columns: columns, spacing: 40) {
-                    ForEach(0 ..< 8, id: \.self) { i in
-                        CostumeCellView(costumeName: costumeNames[i], isSelect: $isSelect)
-                            .sheet(isPresented: $isBottomSheet, content: {
-                                CostumeSheetView(costumeName: $costumeName, isSelect: $isSelect)
-                                .presentationDetents([.medium])
-                                .presentationDragIndicator(.visible)
-                            })
-                            .onTapGesture {
-                                costumeName = costumeNames[i]
-                                isBottomSheet = true
-                            }
+                LazyVGrid(columns: columns, spacing: 2) {
+                    ForEach(cells) { cell in
+                        CostumeCellView(cell: cell, isSheet: false, selectNum: $selectNum)
                     }
                 }
-                .padding(10)
             }
-            .offset(y: 90)
         }
     }
 }
 
 struct CostumeCellView: View {
-    @State var costumeName: String
-    @Binding var isSelect: Bool
+    var cell: CostumeCell
+    @State var isSheet: Bool
+    @Binding var selectNum: Int
     
     var body: some View {
+        
         VStack{
-            Image("\(costumeName)")
+            Image("\(cell.imageName)")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
-                .frame(width: 150, height: 150)
-            Text("\(costumeName)")
-            if isSelect {
-                Text("선택")
-            }
+                .frame(width: 200, height: 200)
+            Text("\(cell.imageTitle)")
+            
+            Text("\(selectNum == cell.cellNum ? "선택" : " ")")
+            
         }
-        }
-    }
-
-struct CostumeSheetView: View {
-    @Binding var costumeName: String
-    @Binding var isSelect: Bool
-    let costumeTitleDict: [String: String] = [
-        "Normal": "클래식",
-        "Cake": "우리 백 일이에요",
-        "Moon": "한 달 살기",
-        "Music": "무병 장수",
-        "Newbie": "시작이 반이다",
-        "Clover": "슬롯 머신",
-        "Apple": "얼리어답터",
-        "Bag1": "돌아온 탕아"
-    ]
-    
-    let costumeExplainDict: [String: String] = [
-        "Normal": "기본으로 제공되는 병아리입니다.",
-        "Cake": "설정한 핸드폰 사용시간을 잘 지킨날이 100일이 되면 지급됩니다.",
-        "Moon": "한 달 연속으로 핸드폰 목표 사용 시간을 잘 지켰을 때 지급됩니다.",
-        "Music": "설정한 핸드폰 사용시간을 일주일 연속으로 잘 지켰을 때 지급됩니다.",
-        "Newbie": "설정한 핸드폰 사용시간을 1일 잘 지키면 지급됩니다.",
-        "Clover": "3일 연속으로 병아리의 스트레스 지수를 33.3%로 유지했을 때 지급됩니다.",
-        "Apple": "발표날 앱을 설치하시면 지급됩니다.",
-        "Bag1": "설정한 핸드폰 사용시간을 지키지 못해 병아리가 처음 가출했을 때 지급됩니다."
-    ]
-    
-    var body: some View {
-        VStack {
-            ZStack{
-                HStack {
-                    Spacer()
-                    Text("\(costumeTitleDict[costumeName]!)")
-                    Spacer()
-                }
-                HStack {
-                    Spacer()
-                    Spacer()
-                    Image(systemName: "x.circle.fill")
-                        .padding()
-                }
-            }
-            VStack {
-                ZStack {
-                    Image("\(costumeName)")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 300, height: 300)
-                    Text("\(costumeExplainDict[costumeName]!)")
-                        .frame(width: 300, height: 300)
-                        .lineLimit(2)
-                        .offset(y: 120)
-                }
-                Button(
-                    action: {
-                        isSelect.toggle()
-                    },
-                    label: {
-                        Text("선택")
-                            .font(.title)
-                    }
-                )
-            }
+        .sheet(isPresented: $isSheet,
+               content: {
+            CostumeSheetView(cell: cell, selectNum: $selectNum, isSheet: $isSheet)
+                .presentationDetents([.medium])
+                .presentationDragIndicator(.visible)
+        })
+        .onTapGesture {
+            isSheet = true
         }
     }
 }
 
+struct CostumeSheetView: View {
+    var cell: CostumeCell
+    @Binding var selectNum: Int
+    @Binding var isSheet: Bool
+    
+    var body: some View {
+        
+        VStack {
+            HStack {
+                Spacer()
+                Button(
+                    action: {
+                        isSheet = false
+                    },
+                    label: {
+                        Image(systemName: "x.circle.fill")
+                            .padding()
+                    }
+                )
+            }
+            
+            Text("\(cell.imageTitle)")
+            Image("\(cell.imageName)")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height: 300)
+            Text("\(cell.imageExplain)")
+            
+            if cell.isUnlocked {
+                Button(
+                    action: {
+                        selectCell()
+                    },
+                    label: {
+                        Text("\(cell.cellNum) ")
+                            .font(.title)
+                        Text("\(selectNum == cell.cellNum ? "선택 취소" : "선택")")
+                            .font(.title)
+                    }
+                )
+            }else {
+                Text("\(cell.cellNum) 를 선택할 수 없습니다.")
+                    .font(.title)
+            }
+        }
+    }
+    
+    private func selectCell() {
+        selectNum = cell.cellNum
+    }
+}
 
 struct CostumesBookView_Previews: PreviewProvider {
     static var previews: some View {
