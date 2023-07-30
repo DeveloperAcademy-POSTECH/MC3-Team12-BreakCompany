07211c7738727413a9e4dcd0e144696c501aa57b
suri0000 <yesle2005@naver.com>
2023-07-30 23:53:01 +0900

    [Feat/#85] SplashView 디자인 반영 완료

--- PeepPeep/PeepPeep/View/SplashScreen/SplashView.swift
+++ PeepPeep/PeepPeep/View/SplashScreen/SplashView.swift
@@ -16,73 +16,93 @@ struct SplashView: View {
         NavigationStack {
             TabView {
                 ZStack {
-                    Text("이상한 아저씨가 학교 앞에서 무엇인가 팔고 있다")
-                        .font(.custom("DOSSaemmul", size: 17))
-                        .lineSpacing(19)
-                        .multilineTextAlignment(.center)
-                        .frame(width: 300)
-                        .offset(y: -200)
+                    VStack{
+                        Text("이상한 아저씨가 학교 앞에서\n무엇인가 팔고 있다")
+                            .font(.custom("DOSSaemmul", size: 17))
+                            .lineSpacing(19)
+                            .multilineTextAlignment(.center)
+                            .frame(width: 300)
+                        
+                        Spacer()
+                            .frame(height: 471)
+                    }
 
                     Image("ChickSeller1")
                         .resizable()
                         .frame(width: 300, height: 300)
-                        .offset(y: 50)
+                        .offset(y: 60)
                 }
 
                 ZStack {
-                    Text("앗 병아리잖아?\n옛날부터 너무 키워보고 싶었는데...")
-                        .font(.custom("DOSSaemmul", size: 17))
-                        .lineSpacing(19)
-                        .multilineTextAlignment(.center)
-                        .frame(width: 300)
-                        .offset(y: -200)
+                    VStack {
+                        Text("앗 병아리잖아?\n옛날부터 너무 키워보고 싶었는데...")
+                            .font(.custom("DOSSaemmul", size: 17))
+                            .lineSpacing(19)
+                            .multilineTextAlignment(.center)
+                            .frame(width: 300)
+                            
+                        Spacer()
+                            .frame(height: 471)
+                    }
 
-                    Image("ChickSeller2")
+                    Image("Chick")
                         .resizable()
-                        .frame(width: 300, height: 300)
-                        .offset(y: 50)
+                        .frame(width: 302, height: 302)
+                        .offset(y: 60)
                 }
 
                 ZStack {
-                    Text("\"병아리는 많이 만지면 아프단다.\"")
-                        .font(.custom("DOSSaemmul", size: 17))
-                        .lineSpacing(19)
-                        .multilineTextAlignment(.center)
-                        .frame(width: 300)
-                        .offset(y: -200)
+                    VStack {
+                        Text("\"병아리는 많이 만지면 아프단다.\"")
+                            .font(.custom("DOSSaemmul", size: 17))
+                            .lineSpacing(19)
+                            .multilineTextAlignment(.center)
+                            .frame(width: 300)
+                        
+                        Spacer()
+                            .frame(height: 486)
+                    }
 
                     Image("ChickSeller3")
                         .resizable()
-                        .frame(width: 300, height: 300)
+                        .frame(width: 397, height: 397)
                         .offset(y: 50)
                 }
 
                 ZStack {
-                    Text("\"핸드폰을 만지는 동안\n병아리가 스트레스를 받으니,\n주의해서 잘 키워주거라!\"")
-                        .font(.custom("DOSSaemmul", size: 17))
-                        .lineSpacing(19)
-                        .multilineTextAlignment(.center)
-                        .frame(width: 300)
-                        .offset(y: -200)
+                    VStack {
+                        Text("\"핸드폰을 만지는 동안\n병아리가 스트레스를 받으니,\n주의해서 잘 키워주거라!\"")
+                            .font(.custom("DOSSaemmul", size: 17))
+                            .lineSpacing(19)
+                            .multilineTextAlignment(.center)
+                            .frame(width: 300)
+                        
+                        Spacer()
+                            .frame(height: 456)
+                    }
 
                     Image("ChickSeller3")
                         .resizable()
-                        .frame(width: 400, height: 400)
+                        .frame(width: 499, height: 499)
                         .offset(y: 50)
                 }
 
                 ZStack {
-                    Text("우리집으로 가장!")
-                        .font(.custom("DOSSaemmul", size: 17))
-                        .lineSpacing(19)
-                        .multilineTextAlignment(.center)
-                        .frame(width: 300)
-                        .offset(y: -200)
+                    VStack {
+                        Text("우리집으로 가장!")
+                            .font(.custom("DOSSaemmul", size: 17))
+                            .lineSpacing(19)
+                            .multilineTextAlignment(.center)
+                            .frame(width: 300)
+                        
+                        Spacer()
+                            .frame(height: 471)
+                    }
 
                     Image("ChickInBox")
                         .resizable()
-                        .frame(width: 300, height: 300)
-                        .offset(y: 50)
+                        .frame(width: 280, height: 280)
+                        .offset(y: 60)
                     VStack {
                         Rectangle().foregroundColor(.clear)
                         Button(action: {
@@ -92,7 +112,8 @@ struct SplashView: View {
                             }
                         }
                         .buttonStyle(CommonButtonStyle(paddingSize: 30))
-                        Spacer(minLength: 115)
+                        
+                        Spacer(minLength: 105)
                     }
                 }
             }
@@ -101,7 +122,7 @@ struct SplashView: View {
             .onAppear {
                 setupAppearance()
             }
-            .padding(.bottom, 50)
+            .padding(.bottom, 31)
         }
     }
 
