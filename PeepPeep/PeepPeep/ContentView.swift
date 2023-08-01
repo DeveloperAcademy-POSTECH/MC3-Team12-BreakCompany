2e4425891d5a63fb2768354de8836fda019ed977
suri0000 <yesle2005@naver.com>
2023-08-01 20:33:10 +0900

    [Feat/#112] LoadingView Fade out

--- PeepPeep/PeepPeep/ContentView.swift
+++ PeepPeep/PeepPeep/ContentView.swift
@@ -9,20 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var isLoading: Bool = true
+    @State var isLoading: CGFloat = 1.0
     
     var body: some View {
         ZStack{
             SplashView()
+                .opacity(isLoading == 0.0 ? 1.0 : 0.0)
+                .animation(nil)
+           
+            LoadingView()
+                .opacity(isLoading)
+                .animation(.easeOut(duration: 1))
             
-            if isLoading {
-                LoadingView()
-            }
         }
         .onAppear {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
-                isLoading.toggle()
-            })
-        }
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                        withAnimation(.easeIn(duration: 1)) {
+                            isLoading = 0.0
+                        }
+                    }
+                }
+//        .onAppear {
+//            withAnimation(.easeIn(duration: 1)) {
+//                isLoading = 0.0
+//            }
+//        }
     }
 }
